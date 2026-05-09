15297137c5229ef37b1470b6ac8dd8189b9ca2df
Shrimanraj L J <shrimanraj.lj@gmail.com>
2026-05-09 22:18:35 +0530

    Updated Flask app configuration portnumber

--- app/main.py
+++ app/main.py
@@ -41,5 +41,6 @@ def apply():
 
         return "Application submitted successfully!"
     return render_template('form.html')
-app.run(host='0.0.0.0',port=80)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
